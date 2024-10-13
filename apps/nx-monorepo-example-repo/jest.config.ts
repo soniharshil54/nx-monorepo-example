4d7e99394953596e31c97844e4e7f277afc522c6
soniharshil54 <sharshil43@yahoo.com>
2024-10-13 18:33:23 +0530

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/jaOTMYxWt0

---
+++ apps/nx-monorepo-example-repo/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nx-monorepo-example-repo',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-monorepo-example-repo',
+};
